67cacd21b098e60e6c0f1a27c60d626bbf1e8dd8
Matias Bellezze <matiasbllzz@gmail.com>
2021-06-03 23:07:24 -0300

    Agregando NavBar malisimamente horrible

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <ul>
+                    <li><a href="">Saurus</a></li>
+                    <li><a href="">Productos</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
